fd2d7c2d3d933ea7bd98615045a3b52b00fe54fc
Mark Knol <mark.knol@gmail.com>
2019-04-18 09:56:35 +0200

    Optimize Haxe code to avoid repeating blocks
     - use `inline` \o/

--- test/Macro.hx
+++ test/Macro.hx
@@ -9,13 +9,46 @@ class Macro{
 
 	public static macro function test() {
 		var input = "aaaaabaaaaacaaaaadaaaaaeaaaaaaa";
+		
 		var l = new LZString();
+		var compressed = l.compress(input);
+		trace(compressed);
+		var decompressed = l.decompress(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
+		var l = new LZString();
+		var compressed = l.compressToEncodedURIComponent(input);
+		trace(compressed);
+		var decompressed = l.decompressFromEncodedURIComponent(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
+		var l = new LZString();
+		var compressed = l.compressToUint8Array(input);
+		trace(compressed);
+		var decompressed = l.decompressFromUint8Array(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
+		var l = new LZString();
+		var compressed = l.compressToBase64(input);
+		trace(compressed);
+		var decompressed = l.decompressFromBase64(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
 		var compressed = l.compressToUTF16(input);
 		trace(compressed);
 		var decompressed = l.decompressFromUTF16(compressed);
 		trace(decompressed);
 		trace(Std.int(compressed.length / input.length * 100) + "%");
 		trace(decompressed == input);
+		
 		return macro $v{compressed};
 	}
 	
--- test/Main.hx
+++ test/Main.hx
@@ -7,17 +7,42 @@ import lzstring.LZString;
 class Main {
 	public static function main() {
 		var input = "aaaaabaaaaacaaaaadaaaaaeaaaaaaa";
+		
 		var l = new LZString();
-		var compressed = l.compressToUTF16(input);
+		var compressed = l.compress(input);
+		trace(compressed);
+		var decompressed = l.decompress(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
+		var l = new LZString();
+		var compressed = l.compressToEncodedURIComponent(input);
+		trace(compressed);
+		var decompressed = l.decompressFromEncodedURIComponent(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
+		var l = new LZString();
+		var compressed = l.compressToUint8Array(input);
+		trace(compressed);
+		var decompressed = l.decompressFromUint8Array(compressed);
+		trace(decompressed);
+		trace(Std.int(compressed.length / input.length * 100) + "%");
+		trace(decompressed == input);
+		
+		var l = new LZString();
+		var compressed = l.compressToBase64(input);
 		trace(compressed);
 		var decompressed = l.decompressFromBase64(compressed);
 		trace(decompressed);
 		trace(Std.int(compressed.length / input.length * 100) + "%");
 		trace(decompressed == input);
 		
-		var compressed = l.decompressFromUTF16(input);
+		var compressed = l.compressToUTF16(input);
 		trace(compressed);
-		var decompressed = l.decompress(compressed);
+		var decompressed = l.decompressFromUTF16(compressed);
 		trace(decompressed);
 		trace(Std.int(compressed.length / input.length * 100) + "%");
 		trace(decompressed == input);
